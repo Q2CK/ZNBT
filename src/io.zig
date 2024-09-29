35e0cfa5c27c6dd95da1b8e81c1df71d0e151320
Q2CK <109765887+Q2CK@users.noreply.github.com>
2024-09-29 18:39:06 +0200

    add SNBT text formatting selection

--- src/io.zig
+++ src/io.zig
@@ -8,7 +8,19 @@ const Tag = tag_import.Tag;
 const TagType = tag_import.TagType;
 
 /// Compression method for the final binary NBT data
-pub const Compression = enum { None, Gzip, Zlib };
+pub const Compression = enum { 
+    None, Gzip, Zlib 
+};
+
+/// Text formatting method for SNBT data 
+pub const SNBTFormat = enum {
+    /// No unnecessary characters
+    Compact,
+    /// Readable single-line formatting
+    SingleLine,
+    /// Readable multi-line formatting
+    MultiLine
+};
 
 /// Writes binary NBT data into the `writer`, using the given `name` and `compound` as the root tag.
 ///
@@ -63,6 +75,9 @@ pub fn readBin(alloc: std.mem.Allocator, path: []const u8) !collections.Compound
 }
 
 /// Writes NBT data in SNBT format into the `writer`, using the given `compound` as the root tag.
-pub fn writeSNBT(compound: collections.Compound, writer: anytype) !void {
+pub fn writeSNBT(compound: collections.Compound, writer: anytype, format: SNBTFormat) !void {
+    // TODO: Implement different text formatting methods
+    _ = format;
+
     try compound.snbt(writer);
 }
